520aecbabfb21b046b6ebdf83037bda5f81b85c8
Niyati Nehal <niyatinehal58@gmail.com>
2022-08-08 23:01:53 +0530

    fles created

---
+++ app.js
@@ -0,0 +1,158 @@
+function reverseStr(str){
+    var listOfChar= str.split('');
+    var reversedListOfChar= listOfChar.reverse();
+
+    var reverseStr=reversedListOfChar.join('');
+    return reverseStr;
+}
+
+function isPalindrome(str){
+    var reverse=reverseStr(str);
+    if(str=== reverse)
+    {
+        return true;
+    }
+    else{
+        return false;
+    }
+
+}
+
+function convertDateToStr(date){
+      var dateStr= {day:'',month:'', year:''};
+      if(date.day<10){
+        dateStr.day='0'+date.day;
+      }
+      else{
+        dateStr.day= date.day.toString();
+      }
+
+      if(date.month<10)
+      {
+        dateStr.month='0'+date.month;
+      }
+      else{
+        dateStr.month=date.month.to
+        dateStr.month=date.month.toString();
+      }
+      
+      dateStr.year=date.year.toString();
+      return dateStr;
+}
+
+function getAllDateFormats(date){
+    var dateStr=convertDateToStr(date);
+
+    var ddmmyyyy= dateStr.day + dateStr.month+ dateStr.year;
+    var mmddyyyy= dateStr.month + dateStr.day + dateStr.year;   
+    var yyyymmdd=dateStr.year +dateStr.month + dateStr.day;
+    var ddmmyy=dateStr.day+ dateStr.month+ dateStr.year.slice(-2);
+    var mmddyy= dateStr.month +dateStr.day +dateStr.year.slice(-2);
+
+    return [ddmmyyyy, mmddyyyy, yyyymmdd, ddmmyy, mmddyy];
+
+}
+
+function checkPalindromeForAllDateFormat(date){
+    var listOfPAlindromes= getAllDateFormats(date);
+    var Palindrome=false;
+    for(var i=0; i<listOfPAlindromes.length;i++)
+    {
+        if(isPalindrome(listOfPAlindromes[i]))
+        
+          {  Palindrome=true;
+            break;
+        }
+    }
+    return Palindrome;
+}
+
+function getNextDate(date)
+{
+    var day=date.day+1;
+    var month=date.month;
+    var year=date.year;
+
+    var daysInMonth=[31,28,31,30,31,30,31,31,30,31,30,31];
+
+    if(month===2)
+    {
+        if(isLeapYear(year))
+        {
+           if(day>29)
+           {
+            day=1;
+            month++;
+           }
+        }
+        else
+        {
+            if(day>28)
+            {
+                day=1;
+                month++; 
+            }
+        }
+
+    }
+    else 
+    {
+        if(day>daysInMonth[month-1])
+        {
+            day=1;
+            month++;
+        }
+    }
+
+    if(month>12)
+    {
+        month=1;
+        year++;
+    }
+
+    return{
+        day:day,
+        month:month,
+        year:year
+    };
+            
+}        
+
+function isLeapYear(year){
+    if(year% 400===0)
+    {
+        return true;
+    }
+    
+    if(year% 100===0)
+    {
+        return false;
+    }
+
+    if(year% 4 ===0)
+    {
+      return true;
+    }
+    return false
+}
+
+function getNextPalindromeDate(date){
+    var ctr=0;
+    var nextDate=getNextDate(date);
+
+    while(1){
+        ctr++;
+        var isPalindrome= checkPalindromeForAllDateFormat(nextDate);
+        if(isPalindrome){
+            break;
+        }
+        nextDate=getNextDate(nextDate);
+    }
+    return [ctr, nextDate];
+}
+
+var date={
+    day:8,
+    month:8,
+    year: 2021
+};
